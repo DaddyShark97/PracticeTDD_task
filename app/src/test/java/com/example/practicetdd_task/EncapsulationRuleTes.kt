e045ef5ff1c3efb4694bd0b6d36b6494f3847c94
DaddyShark97 <shumikdenys@gmail.com>
2023-04-19 10:25:45 +0300

    EncapsulationRuleTest

---
+++ app/src/test/java/com/example/practicetdd_task/EncapsulationRuleTes.kt
@@ -0,0 +1,97 @@
+package com.example.practicetdd_task
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EncapsulationRuleTest {
+
+    @Test
+    fun `encapsulation rule passed`() {
+        val rule: GoodCodeRule = GoodCodeRule.Encapsulation()
+        val textList = listOf(
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "class Repository(private val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    private var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(private val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    protected var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(protected val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    private var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(protected val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    protected var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(protected val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    protected abstract val page: Int\n" +
+                    "}",
+        )
+        textList.forEach { text ->
+            val actual = rule.isValid(text = text)
+            val expected = true
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `encapsulation rule not passed`() {
+        val rule: GoodCodeRule = GoodCodeRule.Encapsulation()
+        val textList = listOf(
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "class Repository(val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "class Repository(private val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "class Repository(val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "   private var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(protected val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "    var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "   protected var page: Int = 0\n" +
+                    "}",
+            "package com.github.johnnysc.practicetdd\n" +
+                    "\n" +
+                    "abstract class Repository(val dataSource: DataSource) {\n" +
+                    "\n" +
+                    "   protected abstract val page: Int\n" +
+                    "}",
+        )
+        textList.forEach { text ->
+            val actual = rule.isValid(text = text)
+            val expected = false
+            assertEquals(expected, actual)
+        }
+    }
+}
